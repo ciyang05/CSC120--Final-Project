031bfcb18eca765b8c1e1fa617f8ab8a33476c79
cyang-05 <chiashiiyang@gmail.com>
2025-04-20 14:20:31 -0400

    new files

---
+++ Room.java
@@ -0,0 +1,12 @@
+public class Room {
+    
+    // Attributes 
+    private String name;
+    private int n;
+    private int e;
+    private int s;
+    private int w;
+    private String description;
+
+    public Room(String rName, )
+}
